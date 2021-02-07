2d328a590b4c007422436444543cac50298f4606
Sazidul_Hoque <rezvee19@outlook.com>
2021-02-07 17:12:11 +0600

    Added arrayStudents.go

---
+++ arrayStudents.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+func main(){
+
+//var students [5] string
+//students[0]="Ahmad"
+//students[1]="Shoyeb"
+//students[3]="Anjona"
+//students[2]="Ibrahim"
+//students[4]="Khalil"
+students := [...] string{"Akib","javed","Mahmud","Showkot","Rakib"}  
+fmt.Println(students)
+fmt.Println(len(students))
+
+}
