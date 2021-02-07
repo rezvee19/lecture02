c87a91bea106273f0cee706a0a90c1261eb46a49
Sazidul_Hoque <rezvee19@outlook.com>
2021-02-07 20:55:06 +0600

    Slice got changed Map inserted

--- slice.go
+++ slice.go
@@ -1,18 +1,27 @@
 package main
 import "fmt"
-import "reflect"
+//import "reflect"
 
 func main(){
-students := [...] string {"Rubel", "Hassan", "Anik", "Karim", "Ovi", "Jahin"}
+//students := [...] string {"Rubel", "Hassan", "Anik", "Karim", "Ovi", "Jahin"}
 
 //sliced:= students[0:4]
-var fruits []string
-fruits= append(fruits,"Apple", "Banana", "Orange","Sapoda")
+//var fruits []string
+//fruits= append(fruits,"Apple", "Banana", "Orange","Sapoda")
 
-fmt.Println(fruits)
-fmt.Printf("%T \n",fruits)
-fmt.Printf("%T \n",students)
-x := reflect.TypeOf(fruits).Kind().String()
-fmt.Println(x)
+//fmt.Println(fruits)
+//fmt.Printf("%T \n",fruits)
+//fmt.Printf("%T \n",students)
+//x := reflect.TypeOf(fruits).Kind().String()
+//fmt.Println(x)
+var mapName map[string]int
+
+mapName = map[string]int{
+    "rsc": 3711,
+    "r":   2138,
+    "gri": 1908,
+    "adg": 912,
+}
+fmt.Println(mapName)
 
 }
